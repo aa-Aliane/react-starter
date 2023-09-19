2ba434e02235bf8ef6fbd81fd6ccd98c042c6d6a
aa-Aliane <aaliane781@gmail.com>
2023-09-19 11:12:59 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+  return (
+    <div className="main">
+      <h1 className="main__title">React starter</h1>
+      <p className="main__description">
+        React Starter is a React project starter that's as easy to use as it is
+        to pronounce. With React Starter, you can say goodbye to boilerplate and
+        hello to productivity. React Starter comes with everything you need to
+        get started building your next React project, including a pre-configured
+        build process, a Sass setup, and a comprehensive test suite. Plus, it
+        includes a number of example components to get you started on the right
+        foot. Whether you're a beginner or a seasoned pro, React Starter is the
+        perfect way to start your next React project. So what are you waiting
+        for? Get started today!
+      </p>
+    </div>
+  );
+}
+
+export default App;
